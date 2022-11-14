fa2b375dc96ecb2c79a1dcef45b95803938e7fc1
katekovaliuk <kkovaliuk@gmail.com>
2022-11-14 23:10:42 +0100

    Update files

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+
+// Закриття з клавіатури
+// УВАГА
+// Наступний функціонал не обов'язковий для здавання завдання, але буде хорошою додатковою практикою.
+
+// Додай закриття модального вікна після натискання клавіші Escape. Зроби так, щоб прослуховування клавіатури було тільки доти, доки відкрите модальне вікно. Бібліотека basicLightbox містить метод для програмного закриття модального вікна.
+
+const galleryRef = document.querySelector(".gallery");
+// console.log(galleryRef);
+
+const imageMarkup = createGalleryItems(galleryItems);
+
+galleryRef.insertAdjacentHTML("beforeend", imageMarkup);
+galleryRef.addEventListener("click", onImageClick);
+
+
+
+function createGalleryItems(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+            <div class="gallery__item">     
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+            </div>
+            `;
+    })
+        .join('');
+ 
+}
+
+function onImageClick(event) {
+    event.preventDefault();
+    console.log(event.target.dataset.source);
+
+    let modalWindow;
+    modalWindow = basicLightbox.create(`
+    <img src="${event.target.dataset.source}"> width ="800" height="600"`);
+
+    modalWindow.show();
+};
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,34 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery");
+// console.log(galleryRef);
+
+const imageMarkup = createGalleryItems(galleryItems);
+
+galleryRef.insertAdjacentHTML("beforeend", imageMarkup);
+// galleryRef.addEventListener("click", onImageClick);
+
+
+function createGalleryItems(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+            <a class="gallery__item" href="${original}">
+                <img ${preview}" alt="${description}" />
+            </a>
+            `;
+    })
+        .join('');
+ 
+}
+
+let gallery = new SimpleLightbox('.gallery a');
+gallery.on('nextImageLoaded.simplelightbox', function (event) {
+    
+	// do something…
+});
+
+// gallery.on('error.simplelightbox', function (e) {
+// 	console.log(e); // some usefull information
+// });
