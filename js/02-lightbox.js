eb55aca4fb2abda2e5d3d8bc968c10721beb53df
katekovaliuk <kkovaliuk@gmail.com>
2022-11-15 23:33:35 +0100

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,7 +1,7 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
 const galleryRef = document.querySelector(".gallery");
 // console.log(galleryRef);
@@ -9,14 +9,14 @@ const galleryRef = document.querySelector(".gallery");
 const imageMarkup = createGalleryItems(galleryItems);
 
 galleryRef.insertAdjacentHTML("beforeend", imageMarkup);
-// galleryRef.addEventListener("click", onImageClick);
+galleryRef.addEventListener("click", onImageClick);
 
 
 function createGalleryItems(galleryItems) {
     return galleryItems.map(({ preview, original, description }) => {
         return `
             <a class="gallery__item" href="${original}">
-                <img ${preview}" alt="${description}" />
+                <img class="gallery__image" src="${preview}" alt="${description}" />
             </a>
             `;
     })
@@ -24,12 +24,14 @@ function createGalleryItems(galleryItems) {
  
 }
 
-let gallery = new SimpleLightbox('.gallery a');
-gallery.on('nextImageLoaded.simplelightbox', function (event) {
-    
-	// do something…
-});
+function onImageClick(event) {
+    event.preventDefault();
+    if (!event.target.classList.contains("gallery__image")) {
+        return;
+    };
 
-// gallery.on('error.simplelightbox', function (e) {
-// 	console.log(e); // some usefull information
-// });
+    const lightbox = new SimpleLightbox(".gallery a", {
+        captionsData: "alt",
+        captionDelay: 250,
+    });
+}
